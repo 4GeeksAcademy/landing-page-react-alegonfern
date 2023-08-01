5ae511719538230b9fa7e4b30df2a023d89039bc
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-01 21:19:58 +0000

    asd

--- src/js/component/NavBar.js
+++ src/js/component/NavBar.js
@@ -13,7 +13,9 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
