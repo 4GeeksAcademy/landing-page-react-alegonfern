c7f3565be462ac894039dd51be55c714b5cb0e18
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-01 21:20:34 +0000

    asd

--- src/js/component/NavBar.js
+++ src/js/component/NavBar.js
@@ -16,17 +16,7 @@ function NavBar() {
             <Nav.Link href="#link">About</Nav.Link>
             <Nav.Link href="#link">Services</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
