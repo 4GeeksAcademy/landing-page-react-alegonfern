a84cb59f959146ed9b6a3dbbbf0c9aa1a83c9e00
user1fernan <fernandamedinajara2@gmail.com>
2023-08-01 17:09:34 -0400

    avance card

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import MyCard from "./cards.jsx";
 
 //include images into your bundle
 
 //create your first component
 
-
 //Create 
 const Home = () => {
 	return (
-		<> 
+		<>
+		
+		
+		<MyCard/> 
 		</>);
 };
 
