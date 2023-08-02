a70b30eed1f55bc439fb96c56ec326f453340acc
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-02 08:33:32 +0000

    act PROPS

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -6,19 +6,19 @@ function MyCard() {
     <div className='row'>
 
     <div className="col-md-3">
-      <Card></Card>
+    <Card imageSrc="https://picsum.photos/id/233/900/400"/>
     </div>
 
     <div className="col-md-3">
-      <Card></Card>
+      <Card imageSrc="https://picsum.photos/id/322/900/400"/>
     </div>
 
     <div className="col-md-3">
-      <Card></Card>
+      <Card imageSrc="https://picsum.photos/id/231/900/400"/>
     </div>
 
     <div className="col-md-3">
-      <Card></Card>
+    <Card imageSrc="https://picsum.photos/id/321/900/400"/>
     </div>
     
     </div>
@@ -26,10 +26,10 @@ function MyCard() {
   </div>
   );
 }
-function Card(){
+function Card(props){
   return (
     <div className="card">
-      <img src="..." className="card-img-top" alt="..."/>
+      <img src={props.imageSrc} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
