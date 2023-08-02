1d26e8bbb7f1c756a46e0a45aebce23f58bad074
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-02 08:24:10 +0000

    act_footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import MyCard from "./cards";
-import NavBar from "./NavBar";
-import Jumbo from "./Jumbo";
+import NavBar from "./navbar";
+import Jumbo from "./jumbo";
+import Footer from "./footer";
+
+
 
 //include images into your bundle
 
 //create your first component
 
 //Create 
-const Home = () => {
+const Home = (props) => {
+
+
+
 	return (
-		<>
+		<>  <NavBar />
 			<div className="container">
 
-				<NavBar />
+
 				<Jumbo />
 				<MyCard />
 
-
-
-			</div>
+			</div>	<Footer />
 		</>
 
 	);
